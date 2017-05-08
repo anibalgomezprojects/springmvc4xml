76499bf6ba9328b005e56545b4b4b964887a3029
anibal gomez <anibalgomez@icloud.com>
2017-05-08 09:56:40 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,17 @@
+Spring MVC 4
+
+Facets
+
+Por defecto al usar con maven-archetypes-webapp
+
+Java Compiler
+
+Java 1.7
+
+Update Project / Para Compilar
+
+Properties > Deployment Assembly > Add > Java Build Path > Maven
+
+Servidor
+
+Tomcat 7
---
+++ src/main/java/com/myapp/anibalprojects/IndexController.java
@@ -0,0 +1,18 @@
+package com.myapp.anibalprojects;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+@RequestMapping("/")
+public class IndexController {
+
+	@RequestMapping(method = RequestMethod.GET)
+	public String index(Model model) {
+		model.addAttribute("saludo", "Hola");
+		return "index";
+	}
+	
+}
